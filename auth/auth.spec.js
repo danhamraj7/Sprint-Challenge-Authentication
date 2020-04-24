acb8adf59a8b68de1c8ac0c6d5c8dc8b4397187d
danhamraj <danhamraj@gmail.com>
2020-04-24 12:50:38 -0400

    wrote test for register and login

--- auth/auth.spec.js
+++ auth/auth.spec.js
@@ -5,3 +5,61 @@ const db = require("../database/dbConfig");
 beforeEach(() => {
   return db("users").truncate();
 });
+
+describe("auth register router", () => {
+  it("should return 201 OK on registration", async () => {
+    const response = await request(server)
+      .post("/api/auth/register")
+
+      .send({
+        username: "ric",
+        password: "flair",
+      });
+
+    expect(response.status).toBe(201);
+  });
+
+  it("should return valid body", async () => {
+    const response = await request(server).post("/api/auth/register").send({
+      username: "ric",
+      password: "flair",
+    });
+
+    expect(response.body).toHaveProperty("username");
+    expect(response.body).toHaveProperty("password");
+  });
+});
+
+describe("auth login router", () => {
+  it("should return a token", async () => {
+    let response = await request(server).post("/api/auth/register").send({
+      username: "Charlotte",
+      password: "flair",
+    });
+
+    expect(response.status).toBe(201);
+
+    response = await request(server).post("/api/auth/login").send({
+      username: "Charlotte",
+      password: "flair",
+    });
+
+    expect(response.body).toHaveProperty("token");
+  });
+
+  it("should return a 404 if credentials are incorrect ", async () => {
+    let response = await request(server).post("/api/auth/register").send({
+      username: "Charlotte",
+      password: "flair",
+    });
+
+    expect(response.status).toBe(201);
+
+    response = await request(server).post("/api/auth/login").send({
+      username: "Charlotte",
+      password: "fair56",
+    });
+
+    expect(response.status).toBe(404);
+  });
+});
